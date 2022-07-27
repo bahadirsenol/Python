5e1a05b76314840ce7d6eebcb8f7aaefdc07fb94
bahadirsenol <senolbhdr54@gmail.com>
2022-07-27 10:47:23 +0300

    Create SORULAR.py

---
+++ SORULAR.py
@@ -0,0 +1,13 @@
+print("Merhaba Dünya")
+
+isim=input("İsminiz Nedir")
+print("Merhaba",isim)
+
+a=input("1.SAYIYI GİRİNİZ")
+b=input("2.SAYIYI GİRİNİZ")
+print("Sonuç: ",int(a)+int(b))
+
+sayi1=input("1. SAYIYI GİRİNİZ")
+sayi2=input("2.SAYIYI GİRİNİZ")
+print((int(sayi1)+int(sayi2))/2)
+
