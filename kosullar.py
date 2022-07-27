809dfc2d00796cfe659958c794fcb91bd1045f8b
bahadirsenol <senolbhdr54@gmail.com>
2022-07-27 13:03:16 +0300

    Create kosullar.py

---
+++ kosullar.py
@@ -0,0 +1,18 @@
+'''a=input("KuLLANICI ADI GİRİN")
+b=input("ŞİFRE GİRİNİZ  ")
+print(a=="Fatih" and b=="1453")
+
+a=input("Şu anki bulunduğunuz yılı girniz")
+print("569 yıl geçmiş")'''
+
+a=int(input("Notunuzu Girin"))
+if(a<=100 and a>=85):
+    print("Pekiyi")
+if(a<=84 and a>=70):
+    print("iyi") 
+if(a<=59 and a>=50):
+    print("Orta")
+if(a<=49 and a>=25):
+    print("Çalışmalısın")         
+if(a<=24 and a>=0):
+    print("Sanayiye Gitmelisin")          
